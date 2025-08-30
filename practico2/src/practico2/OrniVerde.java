cab6c869037f75eff0c5b6e28c3898eb28f610c3
SantiagoGirardiii <santiagogirardiii15@gmail.com>
2025-08-30 14:39:41 -0300

    Create OrniVerde.java

---
+++ practico2/src/practico2/OrniVerde.java
@@ -0,0 +1,35 @@
+
+package practico2;
+
+import java.util.Scanner;
+
+
+public class OrniVerde extends Castor implements MamaPata {
+    
+    Scanner leer = new Scanner(System.in);
+
+    public OrniVerde(int cola, int velocidad) {
+        super(cola, velocidad);
+    }
+
+    @Override
+
+    public void tocarOrgano() {
+        System.out.println("Do-Re-Mi");
+        leer.next();
+        System.out.println("Fa-Sol-La-Si");
+
+    }
+
+    public void tocarGuitorgan(){
+        super.tocarGuitarra();
+        tocarOrgano();
+        System.out.println("cuac cuaac...!");
+               
+    }
+    
+    
+    
+    
+    
+}
